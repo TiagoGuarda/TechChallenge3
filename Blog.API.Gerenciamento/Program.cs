f3c6c53b38482aca39ba29991e07b3b2f3f71bc0
Tiago Guarda <tiago.guarda@gmail.com>
2023-11-06 22:50:29 -0300

    Adicionado suporte para o application insights

--- Blog.API.Gerenciamento/Program.cs
+++ Blog.API.Gerenciamento/Program.cs
@@ -14,6 +14,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors();
 builder.Services.AddControllers();
 
+// Application Insights
+builder.Services.AddApplicationInsightsTelemetry();
+
 // Serviços e Repositórios
 builder.Services.AddScoped<INoticiaService, NoticiaService>();
 builder.Services.AddScoped<INoticiaRepository, NoticiaRepository>();
